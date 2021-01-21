4a61730151518e886587265716a929dfe2b29989
pragadaarun <pmaniarunkumar@gmail.com>
2021-01-21 17:02:14 +0530

    Arithmetic Operaions into Dictionary and to Array

---
+++ arithArray.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+read -p "Enter First value: " a
+read -p "Enter Second value: " b
+read -p "Enter Third value: " c
+
+declare -A Results
+
+operation1=$((a+b*c))
+operation2=$((a*b+c))
+operation3=$((c*a/b))
+operation4=$((a%b+c))
+
+Results[result1]=$operation1
+Results[result2]=$operation2
+Results[result3]=$operation3
+Results[result4]=$operation4
+
+echo "The Dictionary Contains Values : ${Results[@]} "
+
+for result in ${Results[@]}
+do
+	resultsArray[index++]=$result
+done
+
+echo "The Array contains Element: ${resultsArray[@]}"
